18bd73e090be30fef8bce3c76b7b3a8991870e1c
Алмаз <almgal353@gmail.com>
2022-10-03 14:20:31 +0500

    Initial Commit.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+/**
+ * It prints the sum of three numbers.
+ *
+ * @param args Array<String>
+ */
+fun main(args: Array<String>) {
+	
+	// Try adding program arguments via Run/Debug configuration.
+	// Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+	println("Program arguments: ${args.joinToString()}")
+}
