0532ec3e31bc538640da1db78fdf0995e57e3b8f
Алмаз <almgal353@gmail.com>
2022-10-03 14:48:45 +0500

    Updated logic. Prepared for data split.

--- src/main/kotlin/controller/dataGetter/DocumentDownloader.kt
+++ src/main/kotlin/controller/dataGetter/DocumentDownloader.kt
@@ -1,16 +1,16 @@
-package dataGetter
+package controller.dataGetter
 
 import org.apache.commons.io.FileUtils
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import java.io.File
 import java.io.IOException
-import java.lang.Exception
 import java.net.MalformedURLException
 import java.net.URL
 
 /**
  * Rewritten on Kotlin.
+ *
+ * Base version in Java has been splatted into two classes:
+ * DataGetter -> DocumentDownloader, SiteParser.
  * ```
  * Class, intended to get documents from college official site for parse it.
  * Contains base original logic of document parsing and must be saved.
@@ -19,59 +19,17 @@ import java.net.URL
  * @author Locked15.
  */
 @Suppress("Unused")
-class DataGetter {
-	
-	// region Class Data
-	
-	/**
-	 * Object with static data of this class.
-	 */
+class DocumentDownloader {
+
 	companion object {
-		
-		/**
-		 * A non-breaking space.
-		 */
-		const val NON_BREAKING_SPACE = "\u00A0"
-		
-		/**
-		 * Path to official college website, targeted to changes page.
-		 */
-		const val COLLEGE_CHANGES_PAGE_PATH = "https://www.uksivt.ru/zameny"
 		
 		/**
 		 * A template for a download link to a document on Google Drive.
 		 */
 		const val GOOGLE_DRIVE_LINK_TEMPLATE = "https://drive.google.com/uc?export=download&id="
-		
-		/**
-		 * A CSS selector to the section with documents on the college official site.
-		 */
-		const val CSS_SELECTOR_TO_DOCUMENTS_SECTION = "section > div > div > div > div > div > div " +
-			"> div > div > div > div > div > div > div > div > div > div > div"
 	}
 	
-	/**
-	 * WebPage, connected to college official site.
-	 */
-	private val webPage: Document?
-	// endregion
-	
-	init {
-		var document: Document? = null
-		
-		try {
-			document = Jsoup.connect(COLLEGE_CHANGES_PAGE_PATH).get()
-		}
-		catch (e: Exception) {
-			document = null
-			println("In connection process, an error occurred: ${e.message}.")
-		}
-		finally {
-			webPage = document
-		}
-	}
-	
-	// region Functions
+	/* region Functions */
 	
 	/**
 	 * Merge an original document link and Google Drive a template link.
@@ -88,13 +46,13 @@ class DataGetter {
 	}
 	
 	/**
-	 * Downloads a file with changes from url to the targeted directory.
+	 * Downloads a file from url to the targeted directory.
 	 *
-	 * @param url Ready-To-Download url with a changes document.
+	 * @param url Ready-To-Download url with a document.
 	 * @param path Path to save a downloaded document.
 	 * @return Result of document download.
 	 */
-	fun downloadFileWithChanges(url: String, path: String): Boolean {
+	fun downloadGoogleFile(url: String, path: String): Boolean {
 		try {
 			FileUtils.copyURLToFile(URL(url), File(path))
 			
@@ -109,5 +67,5 @@ class DataGetter {
 		
 		return false
 	}
-	//endregion
+	/* endregion */
 }
