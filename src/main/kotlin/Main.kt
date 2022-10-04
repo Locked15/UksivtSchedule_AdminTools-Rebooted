051b889676c869e52f9ddb7377e9505b69f82505
Алмаз <almgal353@gmail.com>
2022-10-05 02:52:03 +0500

    Updated *.iml-file. Updated (small) Main.kt file.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
+
 /**
- * It prints the sum of three numbers.
+ * Entry point of the program.
  *
- * @param args Array<String>
+ * @param args Arguments of the program.
  */
 fun main(args: Array<String>) {
-	
 	// Try adding program arguments via Run/Debug configuration.
 	// Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
 	println("Program arguments: ${args.joinToString()}")
