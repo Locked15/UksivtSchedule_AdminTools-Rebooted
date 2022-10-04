95c186fd041079599cf60530124fc40a994144c9
Алмаз <almgal353@gmail.com>
2022-10-05 03:21:45 +0500

    WeekSchedule model added. Now, on Kotlin, it's just a header and an additional constructor.

---
+++ src/main/kotlin/model/elements/schedule/WeekSchedule.kt
@@ -0,0 +1,20 @@
+package model.elements.schedule
+
+
+/**
+ * Class, that represents whole week schedule.
+ *
+ * It indented to identify group schedule, so it contains [property][groupName] with group name.
+ */
+class WeekSchedule(var groupName: String?, var daySchedules: List<DaySchedule>) {
+	
+	/* region Constructors */
+	
+	/**
+	 * Empty constructor.
+	 *
+	 * Write 'NULL' value to [groupName], and empty list to [daySchedules].
+	 */
+	constructor() : this(null, listOf<DaySchedule>())
+	/* endregion */
+}
