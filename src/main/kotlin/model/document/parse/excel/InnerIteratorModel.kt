a8571c7fa683fb7731b823db72507032f0a3c9d2
Алмаз <almgal353@gmail.com>
2022-10-11 20:27:50 +0500

    Created "InnerIteratorModel" for Excel document parsing. It placed inside the second cycle of parse function.

---
+++ src/main/kotlin/model/document/parse/excel/InnerIteratorModel.kt
@@ -0,0 +1,20 @@
+package model.document.parse.excel
+
+import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.Row
+import model.element.schedule.base.Lesson
+
+
+/**
+ * Wrapper-Model for Excel document parse.
+ * These properties must be placed inside the second cycle.
+ *
+ * @param lessonNameIsAdded  Determine [lesson name][Lesson.name] is added to a current generating [lesson][Lesson].
+ * @param lessonPlaceIsAdded  Determine [lesson place][Lesson.place] is added to a current generating [lesson][Lesson].
+ * @param dayEndingRow  Contains position of the current day final row.
+ *                      Wrapped from long expression to prettify source code.
+ * @param cells  Contains [all][Iterator] [cells][Cell] inside [current row][Row].
+ *               Simplifies work with current row cells (and probably save memory).
+ */
+class InnerIteratorModel(var lessonNameIsAdded: Boolean, var lessonPlaceIsAdded: Boolean, val dayEndingRow: Int?,
+                         val cells: Iterator<Cell>)
