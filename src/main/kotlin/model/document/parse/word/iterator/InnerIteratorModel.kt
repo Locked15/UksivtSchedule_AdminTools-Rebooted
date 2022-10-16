a40365bda169dee3d6080fe85f33583c35ecb1b6
Алмаз <almgal353@gmail.com>
2022-10-16 22:20:29 +0500

    Inner iterator model for document parsing function.
    It uses inside third (and last) cycle of document parsing. Just for cleaning code.

---
+++ src/main/kotlin/model/document/parse/word/iterator/InnerIteratorModel.kt
@@ -0,0 +1,11 @@
+package model.document.parse.word.iterator
+
+
+/**
+ * Represents a third iteration level (inner) data model of Word document parse.
+ * This one must be placed inside the third cycle.
+ *
+ * @param text  Text of current parsing cell.
+ * @param lowerText  Lowered text of current parsing cell.
+ */
+class InnerIteratorModel(val text: String, val lowerText: String)
