59fdfdc713fbd1a45a0c26fcd03a104b5b09c7fa
Алмаз <almgal353@gmail.com>
2022-10-30 18:01:16 +0500

    Added new writer class. This class targets on changes writing.

---
+++ src/main/kotlin/controller/io/ChangesWriter.kt
@@ -0,0 +1,41 @@
+package controller.io
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import model.element.schedule.Changes
+import java.io.FileWriter
+
+
+/* region Properties */
+
+private const val FILE_NAME_TEMPLATE = "%b.json"
+/* endregion */
+
+/* region Functions */
+
+fun writeChanges(changes: Changes): Boolean {
+    return try {
+        writeToFile(changes)
+    }
+    catch (e: Exception) {
+        println("\n\nOn writing error occurred: ${e.message}.")
+        false
+    }
+}
+
+private fun writeToFile(changes: Changes): Boolean {
+    val serializer = ObjectMapper()
+    val serializedValue = serializer.writerWithDefaultPrettyPrinter().writeValueAsString(changes)
+    return try {
+        val stream = FileWriter("D:\\Java-Projects\\Schedule-AdminTools\\src\\main\\resources\\${
+            String.format(FILE_NAME_TEMPLATE, changes.absolute)
+        }")
+        stream.write(serializedValue)
+        stream.close()
+
+        true
+    }
+    catch (e: Exception) {
+        false
+    }
+}
+/* endregion */
