e9c231a086e19e3922046a43bf9a95c843e63b6c
Алмаз <almgal353@gmail.com>
2022-10-31 02:31:20 +0500

    SiteParser.kt updated. Some bugs with parsing process have been fixed.

--- src/main/kotlin/controller/data/getter/SiteParser.kt
+++ src/main/kotlin/controller/data/getter/SiteParser.kt
@@ -26,205 +26,225 @@ import java.lang.Exception
  */
 class SiteParser {
 
-	/* region Properties */
+    /* region Properties */
 
-	/**
-	 * WebPage, connected to college official site.
-	 */
-	private val webPage: Document?
-	/* endregion */
+    /**
+     * WebPage, connected to college official site.
+     */
+    private val webPage: Document?
+    /* endregion */
 
-	/* region Initializers */
+    /* region Initializers */
 
-	/**
-	 * Tries to connect to the site of college, and if connection is successful, write loaded DOM to field.
-	 * Otherwise, write 'NULL'.
-	 */
-	init {
-		var document: Document? = null
+    /**
+     * Tries to connect to the site of college, and if connection is successful, write loaded DOM to field.
+     * Otherwise, write 'NULL'.
+     */
+    init {
+        var document: Document? = null
 
-		try {
-			document = Jsoup.connect(COLLEGE_CHANGES_PAGE_PATH).get()
-		}
-		catch (e: Exception) {
-			document = null
-			println("In connection process, an error occurred: ${e.message}.")
-		}
-		finally {
-			webPage = document
-		}
-	}
-	/* endregion */
+        try {
+            document = Jsoup.connect(COLLEGE_CHANGES_PAGE_PATH).get()
+        }
+        catch (e: Exception) {
+            document = null
+            println("In connection process, an error occurred: ${e.message}.")
+        }
+        finally {
+            webPage = document
+        }
+    }
+    /* endregion */
 
-	/* region Functions */
+    /* region Functions */
 
-	/**
-	 * Parses college [website](https://uksivt.ru) [page](https://uksivt.ru/zameny)
-	 * and seeks all available [changes nodes][ChangeElement].
-	 *
-	 * It parses [limited count][months] of [months][MonthChanges].
-	 *
-	 * Some Info:
-	 * This function has been written in 2021 year, so it may not include last edits of the site.
-	 * Please [tell](mailto:almgal353@gmail.com) [me](https://bit.ly/3WbRlMn)
-	 * if something [WENT][Exception] [WRONG][Error] on a parsing process.
-	 */
-	fun getAvailableNodes(months: Int = 2): MutableList<MonthChanges> {
-		val base = BaseIteratorModel(0, 0, "Январь", mutableListOf(), mutableListOf())
-		if (webPage != null) {
-			val elements = executePageQueriesAndGetElements()
+    /**
+     * Parses college [website](https://uksivt.ru) [page](https://uksivt.ru/zameny)
+     * and seeks all available [changes nodes][ChangeElement].
+     *
+     * It parses [limited count][months] of [months][MonthChanges].
+     *
+     * Some Info:
+     * This function has been written in 2021 year, so it may not include last edits of the site.
+     * Please [tell](mailto:almgal353@gmail.com) [me](https://bit.ly/3WbRlMn)
+     * if something [WENT][Exception] [WRONG][Error] on a parsing process.
+     */
+    fun getAvailableNodes(months: Int = 2): MutableList<MonthChanges> {
+        val base = BaseIteratorModel(0, 0, "Январь", mutableListOf(), mutableListOf())
+        if (webPage != null) {
+            val elements = executePageQueriesAndGetElements()
             for (element in elements.listOfMonthChangesElements ?: emptyList()) {
                 calculateNewMonthBeginning(element, base)
                 if (element.nodeName() == "table" && base.monthCounter < months) {
-					val table = TableContentWrapper(element.children()[0], element.children()[1],
-													element.children()[1].children())
-	                for (row in table.rows) {
-						val innerIterator = InnerIteratorModel(0, false)
-		                if (row == table.rows.first()) continue
+                    val table = TableContentWrapper(element.children()[0], element.children()[1],
+                                                    element.children()[1].children())
+                    for (row in table.rows) {
+                        val innerIterator = InnerIteratorModel(0, false)
+                        if (row == table.rows.first()) continue
 
-		                iterateAndReadRowCells(row, base, innerIterator)
-	                }
-	                increaseMonth(base)
+                        iterateAndReadRowCells(row, base, innerIterator)
+                    }
+                    increaseMonth(base)
                 }
             }
-		}
+        }
 
-		return base.monthChanges
-	}
+        return base.monthChanges
+    }
 
-	/**
-	 * Executes page queries (like CSS-Selector query) and extracts changes container children.
-	 * Then packs it into a [new object][ChangeElementsWrapper] and returns.
-	 */
-	private fun executePageQueriesAndGetElements(): ChangeElementsWrapper {
-		val containerElement = webPage?.getElementById(PATH_BEGIN_ELEMENT_ID)
-			?.select(CSS_SELECTOR_TO_DOCUMENTS_SECTION)
-			?.get(0)
-		val contentElement = containerElement?.children()
+    /**
+     * Executes page queries (like CSS-Selector query) and extracts changes container children.
+     * Then packs it into a [new object][ChangeElementsWrapper] and returns.
+     */
+    private fun executePageQueriesAndGetElements(): ChangeElementsWrapper {
+        val containerElement = webPage?.getElementById(PATH_BEGIN_ELEMENT_ID)
+            ?.select(CSS_SELECTOR_TO_DOCUMENTS_SECTION)
+            ?.get(0)
+        val contentElement = containerElement?.children()
 
-		return ChangeElementsWrapper(containerElement, contentElement)
-	}
+        return ChangeElementsWrapper(containerElement, contentElement)
+    }
 
-	/**
-	 * Evaluates expressions of a new month beginning.
-	 * Updates relative objects and saves values.
-	 *
-	 * Also called on the first iteration,
-	 * to initialize [some][BaseIteratorModel.changes] [properties][BaseIteratorModel.currentMonth].
-	 */
+    /**
+     * Evaluates expressions of a new month beginning.
+     * Updates relative objects and saves values.
+     *
+     * Also called on the first iteration,
+     * to initialize [some][BaseIteratorModel.changes] [properties][BaseIteratorModel.currentMonth].
+     */
     private fun calculateNewMonthBeginning(element: Element, model: BaseIteratorModel) {
         /* Month-scoped elements always lie inside <p> tag, but in the beginning of the page
            it is space, created by the same tag.
            Also, it contains non-common space (non-breaking one), so we need to check it. */
-        if (element.nodeName() == "p" && element.text() != NON_BREAKING_SPACE) {
+        if (element.nodeName() == "p" && checkValueToMonth(element.text())) {
             // On first iteration we also go here, so we need to check it.
             if (model.changes.isNotEmpty()) model.monthChanges.add(MonthChanges(model.currentMonth, model.changes))
 
             // Then we just reload the content of other properties.
-            model.currentMonth = element.text().substring(0, element.text()
-                .lastIndexOf(' '))
+            model.currentMonth = element.text().substring(0, element.text().lastIndexOf(' '))
                 .replace(NON_BREAKING_SPACE, "")
             model.changes = mutableListOf()
             model.dayOfMonthCounter = 1
         }
     }
 
-	/**
-	 * Iterates [current row][row] [cells][List] and updates [related][base] [objects][inner].
-	 * Cell content parses by [another function][parseCellValueAndUpdateObjects].
-	 */
-	private fun iterateAndReadRowCells(row: Element, base: BaseIteratorModel, inner: InnerIteratorModel) {
-		// All rows after the first, contains target information.
-		for (cell in row.children()) {
-			if (checkFirstCellAndUpdateObjects(cell, inner)) continue
-			else parseCellValueAndUpdateObjects(cell, base, inner)
+    /**
+     * Iterates [current row][row] [cells][List] and updates [related][base] [objects][inner].
+     * Cell content parses by [another function][parseCellValueAndUpdateObjects].
+     */
+    private fun iterateAndReadRowCells(row: Element, base: BaseIteratorModel, inner: InnerIteratorModel) {
+        // All rows after the first, contains target information.
+        for (cell in row.children()) {
+            if (checkFirstCellAndUpdateObjects(cell, inner)) continue
+            else parseCellValueAndUpdateObjects(cell, base, inner)
 
-			increaseIterators(base, inner)
-		}
-	}
+            increaseIterators(base, inner)
+        }
+    }
 
-	/**
-	 * Checks first row cell and updates objects.
-	 *
-	 * The first cell contains a non-breaking space for indentation goal, so we'll have to skip it.
-	 * If new month begins on non-monday, so we'll have to skip more than one cell.
-	 */
-	private fun checkFirstCellAndUpdateObjects(cell: Element, iterator: InnerIteratorModel): Boolean {
-		var result = false
-		if (cell.text() == NON_BREAKING_SPACE) {
-			// If we don't meet at least one day, we'll continue to skip cells.
-			if (!iterator.isFirstIteration) {
-				iterator.dayOfWeekCounter++
-			}
-			// So, to skip some cells, we declare value and continue the cycle.
-			iterator.isFirstIteration = false
-			result = true
-		}
+    /**
+     * Checks first row cell and updates objects.
+     *
+     * The first cell contains a non-breaking space for indentation goal, so we'll have to skip it.
+     * If new month begins on non-monday, so we'll have to skip more than one cell.
+     */
+    private fun checkFirstCellAndUpdateObjects(cell: Element, iterator: InnerIteratorModel): Boolean {
+        var result = false
+        if (getBasicSiblingIndex(cell.siblingIndex()) == 0) {
+            // So, to skip some cells, we declare value and continue the cycle.
+            iterator.isFirstIteration = false
+            result = true
+        }
 
-		return result
-	}
+        return result
+    }
 
-	/**
-	 * Parses cell value, and, if it contains something, writes it to a [related][model] [objects][inner].
-	 * Although it writes empty value, that can be skipped later.
-	 */
-	private fun parseCellValueAndUpdateObjects(cell: Element, model: BaseIteratorModel, inner: InnerIteratorModel) {
-		if (cell.children().size < 1) {
-			model.changes.add(ChangeElement(Day.getValueByIndex(inner.dayOfWeekCounter), -1, null))
-		}
-		else {
-			val link = cell.children().first()
-			model.changes.add(ChangeElement(Day.getValueByIndex(inner.dayOfWeekCounter), model.dayOfMonthCounter,
-											link?.attr("href")))
-		}
-	}
+    /**
+     * Parses cell value, and, if it contains something, writes it to a [related][model] [objects][inner].
+     * Although it writes empty value, that can be skipped later.
+     */
+    private fun parseCellValueAndUpdateObjects(cell: Element, model: BaseIteratorModel, inner: InnerIteratorModel) {
+        if (cell.children().size < 1) {
+            model.changes.add(ChangeElement(Day.getValueByIndex(inner.dayOfWeekCounter), -1, null))
+        }
+        else {
+            val link = cell.children().first()
+            model.changes.add(ChangeElement(Day.getValueByIndex(inner.dayOfWeekCounter), model.dayOfMonthCounter,
+                                            link?.attr("href")))
+        }
+    }
 
-	/**
-	 * Increases month counter.
-	 * If the current month is december (12), sets it to january (1).
-	 *
-	 * Nor more, nor less.
-	 */
-	private fun increaseMonth(base: BaseIteratorModel) {
-		if (base.monthCounter < 12) base.monthCounter++
-		else base.monthCounter = 1
-	}
+    /**
+     * Increases month counter.
+     * If the current month is december (12), sets it to january (1).
+     *
+     * Nor more, nor less.
+     */
+    private fun increaseMonth(base: BaseIteratorModel) {
+        if (base.monthCounter < 12) base.monthCounter++
+        else base.monthCounter = 1
+    }
 
-	/**
-	 * Increases common [iterator][BaseIteratorModel.dayOfMonthCounter] [values][InnerIteratorModel.dayOfWeekCounter].
-	 *
-	 * Nor more, nor less.
-	 */
-	private fun increaseIterators(base: BaseIteratorModel, inner: InnerIteratorModel) {
-		base.dayOfMonthCounter++
-		inner.dayOfWeekCounter++
-	}
-	/* endregion */
+    /**
+     * Increases common [iterator][BaseIteratorModel.dayOfMonthCounter] [values][InnerIteratorModel.dayOfWeekCounter].
+     *
+     * Nor more, nor less.
+     */
+    private fun increaseIterators(base: BaseIteratorModel, inner: InnerIteratorModel) {
+        base.dayOfMonthCounter++
+        inner.dayOfWeekCounter++
+    }
+    /* endregion */
 
-	/* region Companion */
+    /* region Companion */
 
-	companion object {
+    companion object {
 
-		/**
-		 * A non-breaking space.
-		 */
-		const val NON_BREAKING_SPACE = "\u00A0"
+        /* region Constants */
 
-		/**
-		 * Path to official college website, targeted to changes page.
-		 */
-		const val COLLEGE_CHANGES_PAGE_PATH = "https://www.uksivt.ru/zameny"
+        /**
+         * A non-breaking space.
+         */
+        const val NON_BREAKING_SPACE = "\u00A0"
 
-		/**
-		 * ID of the element that used as begin of CSS-Selector selection path.
-		 */
-		const val PATH_BEGIN_ELEMENT_ID = "inside-page"
+        /**
+         * Path to official college website, targeted to changes page.
+         */
+        const val COLLEGE_CHANGES_PAGE_PATH = "https://www.uksivt.ru/zameny"
 
-		/**
-		 * A CSS selector to the section with documents on the college official site.
-		 */
-		const val CSS_SELECTOR_TO_DOCUMENTS_SECTION = "section > div > div > div > div > div > div " +
-			"> div > div > div > div > div > div > div > div > div > div > div"
-	}
-	/* endregion */
+        /**
+         * ID of the element that used as begin of CSS-Selector selection path.
+         */
+        const val PATH_BEGIN_ELEMENT_ID = "inside-page"
+
+        /**
+         * A CSS selector to the section with documents on the college official site.
+         */
+        const val CSS_SELECTOR_TO_DOCUMENTS_SECTION = "section > div > div > div > div > div > div " +
+                "> div > div > div > div > div > div > div > div > div > div > div"
+        /* endregion */
+
+        /* region Functions */
+
+        /**
+         * Calculates [basic][Int] [sibling index][Element.siblingIndex] for the [element][Element].
+         *
+         * Cause final index calculates by formula: "{index} * 2 + 1";
+         * So I reversed it and the calculus basic index by follow: "({final_index} - 1) / 2".
+         */
+        private fun getBasicSiblingIndex(index: Int) = (index - 1) / 2
+
+        /**
+         * Checks [current value][value] to be one of the months.
+         * [Returns][Boolean] 'true' if yes, and 'false' otherwise.
+         */
+        private fun checkValueToMonth(value: String) = when (value.lowercase()) {
+            "январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь",
+            "ноябрь", "декабрь" -> true
+
+            else -> false
+        }
+        /* endregion */
+    }
+    /* endregion */
 }
