d9e8d3a5c7377bf7e0bd4b339c26230ab02d3bf5
Locked15 <almgal353@gmail.com>
2023-01-09 20:16:14 +0500

    Changes now contain "toString()" function.

--- src/main/kotlin/model/data/schedule/Changes.kt
+++ src/main/kotlin/model/data/schedule/Changes.kt
@@ -9,20 +9,47 @@ import model.data.schedule.base.Lesson
  * Earlier, it wasn't a class, just a combination of list with changes and boolean with absolute determination.
  */
 class Changes(val changedLessons: MutableList<Lesson>, var absolute: Boolean) {
-	
-	/* region Constructors */
-	
-	/**
-	 * Additional constructor, that write default values to properties.
-	 *
-	 * Info: [changedLessons] set to an empty list, [absolute] to false.
-	 */
-	constructor() : this(mutableListOf<Lesson>(), false)
-	
-	/**
-	 * Additional constructor, that write changes to property.
-	 * Another property ([absolute]) set to false.
-	 */
-	constructor(changes: MutableList<Lesson>) : this(changes, false)
-	/* endregion */
+
+    /* region Constructors */
+
+    /**
+     * Additional constructor, that write default values to properties.
+     *
+     * Info: [changedLessons] set to an empty list, [absolute] to false.
+     */
+    constructor() : this(mutableListOf<Lesson>(), false)
+    /* endregion */
+
+    /* region Functions */
+
+    /**
+     * Returns string representation of the current object.
+     */
+    override fun toString(): String {
+        val builder = StringBuilder(changedLessons.size)
+        for (lesson in changedLessons) {
+            builder.append("${lesson.number} — ${lesson.name}.")
+        }
+
+        return String.format(STRING_BODY_TEMPLATE, absolute, builder.toString())
+    }
+    /* endregion */
+
+    /* region Companion */
+
+    companion object {
+
+        /**
+         * Contains template for string-formatting instances of [Changes] objects.
+         */
+        const val STRING_BODY_TEMPLATE =
+            """
+                IsAbsolute: %b;
+                Changes:
+                {
+                    %s.
+                }
+            """
+    }
+    /* endregion */
 }
