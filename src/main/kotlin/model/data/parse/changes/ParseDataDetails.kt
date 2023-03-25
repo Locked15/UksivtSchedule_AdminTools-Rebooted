38a3a1693a6c9f9c0ee5d660c3cff100d4f31f3c
Locked15 <almgal353@gmail.com>
2023-03-26 01:20:08 +0500

    Added wrapper for changes parse process, instead basic 'Triple' class.

---
+++ src/main/kotlin/model/data/parse/changes/ParseDataDetails.kt
@@ -0,0 +1,6 @@
+package model.data.parse.changes
+
+import model.data.schedule.base.day.Day
+
+
+class ParseDataDetails(val pathToFile: String, val targetGroup: String?, val isAutoMode: Boolean, val targetDay: Day?)
