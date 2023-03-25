49a2aeea342c701ff062f6bea6db7f63948afdde
Locked15 <almgal353@gmail.com>
2023-03-26 01:21:28 +0500

    Added new constructor to the 'Lesson' class. It's a back compatibility constructor (initializes a new Lesson with all-data, except 'isChanged' property).

--- src/main/kotlin/model/data/schedule/base/Lesson.kt
+++ src/main/kotlin/model/data/schedule/base/Lesson.kt
@@ -9,54 +9,65 @@ package model.data.schedule.base
  * @param teacher This lesson teacher (written in a short format if it possible).
  * @param place Place, where this lesson would be arranged (in a string format).
  */
-class Lesson(var number: Int?, var name: String?, var teacher: String?, var place: String?) : Comparable<Lesson> {
-	
-	/* region Constructors */
-	
-	/**
-	 * Default constructor of the class.
-	 * Created to make code simpler to understand.
-	 *
-	 * Write all properties as 'NULL'.
-	 */
-	constructor() : this(null, null, null, null)
-	
-	/**
-	 * Constructor, that initialize lesson number.
-	 * Can be found useful, when you need late property initialization.
-	 *
-	 * Sets the [number] property to argument value.
-	 * Other properties are set to 'NULL'.
-	 */
-	constructor(number: Int?) : this(number, null, null, null)
-	
-	/**
-	 * Constructor that initializes lesson number and name.
-	 * Can be useful, when you need to work with 'Practise' or 'Credit' schedules.
-	 *
-	 * Sets the [number] and [name] property to arguments value.
-	 * Other properties are set to 'NULL'.
-	 */
-	constructor(number: Int, name: String) : this(number, name, null, null)
-	/* endregion */
-	
-	/* region Functions */
-	
-	/**
-	 * Allow comparing two instances of this class.
-	 *
-	 * @param other Another instance to compare with current.
-	 */
-	override fun compareTo(other: Lesson): Int {
-		return if (number!! > other.number!!) {
-			1
-		}
-		else if (number!! == other.number!!) {
-			0
-		}
-		else {
-			-1
-		}
-	}
-	/* endregion */
+class Lesson(var number: Int?, var name: String?, var teacher: String?, var place: String?, var isChanged: Boolean) :
+        Comparable<Lesson> {
+
+    /* region Constructors */
+
+    /**
+     * Default constructor of the class.
+     * Created to make code simpler to understand.
+     *
+     * Write all properties as 'NULL'.
+     */
+    constructor() : this(null, null, null, null, false)
+
+    /**
+     * Constructor, that initialize lesson number.
+     * Can be found useful, when you need late property initialization.
+     *
+     * Sets the [number] property to argument value.
+     * Other properties are set to 'NULL'.
+     */
+    constructor(number: Int?) : this(number, null, null, null, false)
+
+    /**
+     * Constructor that initializes lesson number and name.
+     * Can be useful, when you need to work with 'Practise' or 'Credit' schedules.
+     *
+     * Sets the [number] and [name] property to arguments value.
+     * Other properties are set to 'NULL'.
+     */
+    constructor(number: Int, name: String) : this(number, name, null, null, false)
+
+    /**
+     * Back compatibility.
+     * To not update all instances with 'false' in 'isChanged' property.
+     *
+     * Sets the [number] and [name] property to arguments value.
+     * Other properties are set to 'NULL'.
+     */
+    constructor(number: Int?, name: String?, teacher: String?, place: String?) : this(number, name, teacher, place,
+                                                                                      false)
+    /* endregion */
+
+    /* region Functions */
+
+    /**
+     * Allow comparing two instances of this class.
+     *
+     * @param other Another instance to compare with current.
+     */
+    override fun compareTo(other: Lesson): Int {
+        return if (number!! > other.number!!) {
+            1
+        }
+        else if (number!! == other.number!!) {
+            0
+        }
+        else {
+            -1
+        }
+    }
+    /* endregion */
 }
