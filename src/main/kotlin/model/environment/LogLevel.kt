38bb457165420d12c172d99a8a7e7fe9fc47b19d
Locked15 <almgal353@gmail.com>
2023-03-29 12:35:25 +0500

    Added basement for further logger updates.

---
+++ src/main/kotlin/model/environment/LogLevel.kt
@@ -0,0 +1,14 @@
+package model.environment
+
+enum class LogLevel {
+
+    CRITICAL,
+
+    ERROR,
+
+    WARNING,
+
+    DEBUG,
+
+    INFORMATION
+}
---
+++ src/main/kotlin/model/environment/Logger.kt
@@ -0,0 +1,28 @@
+package model.environment
+
+
+class Logger {
+
+    companion object {
+
+        fun logCritical() {
+
+        }
+
+        fun logError() {
+
+        }
+
+        fun logWarning() {
+
+        }
+
+        fun logDebug() {
+
+        }
+
+        fun showInformation() {
+
+        }
+    }
+}
