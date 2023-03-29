433e31a365edd051b6ac7c19635ac3970929375f
Locked15 <almgal353@gmail.com>
2023-03-29 16:17:02 +0500

    Main class updated to work with GlobalState object.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
+import model.environment.state.GlobalState
+import model.environment.state.GlobalStateBuilder
 import view.console.Basic
 
 
-val projectDirectory: String = System.getProperty("user.dir")
+private var programArgs: Array<String> = arrayOf()
 
-val resourcePathElements = listOf("src", "main", "resources")
+val globalState: GlobalState by lazy {
+    val builder = GlobalStateBuilder()
+        .setProjectDirectory(System.getProperty("user.dir"))
+        .setByArgs(programArgs)
+
+    builder.build()
+}
 
 /**
  * Entry point of the program.
@@ -11,8 +19,13 @@ val resourcePathElements = listOf("src", "main", "resources")
  * @param args Arguments of the program.
  */
 fun main(args: Array<String>) {
+    programArgs = args.map { it.lowercase() }.toTypedArray()
+
     print("Enter your name: ")
     Basic(readln()).beginSession()
+    endSession()
+}
 
+private fun endSession() {
     println("\nConnection terminated.\nGood day.")
 }
