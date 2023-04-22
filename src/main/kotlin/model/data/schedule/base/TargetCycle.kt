75073fa5c6335e9bedeb25626894ae54a69b6049
Locked15 <almgal353@gmail.com>
2023-04-22 08:16:30 +0500

    Added new entity class.

---
+++ src/main/kotlin/model/data/schedule/base/TargetCycle.kt
@@ -0,0 +1,39 @@
+package model.data.schedule.base
+
+import model.entity.schedule.lite.base.TargetCycle as TargetCycleEntity
+import org.jetbrains.exposed.dao.id.EntityID
+
+
+class TargetCycle(private val year: Int, private val semester: Int) {
+
+    fun getTargetCycleId(): EntityID<Int>? {
+        val allCycles = TargetCycleEntity.all().toList()
+        return allCycles.firstOrNull { it.year == year && it.semester == semester }?.id
+    }
+
+    companion object {
+
+        private const val DEFAULT_YEAR = 2023
+
+        private const val DEFAULT_SEMESTER = 2
+
+        fun createInstanceByStringQuery(query: String?): TargetCycle {
+            val values = query?.split('/')
+            return if (values != null && values.size == 2) {
+                val year = values[0].toIntOrNull()
+                val semester = values[1].toIntOrNull()
+                if (year == null || semester == null) {
+                    println("WARNING:\n\tOne of values on TargetCycle creation was 'NULL'." +
+                                    "\n\tDefault values will be used.")
+                }
+
+                TargetCycle(year ?: DEFAULT_YEAR, semester ?: DEFAULT_SEMESTER)
+            }
+            else {
+                println("WARNING:\n\tTargetCycle creation by string query was faulted." +
+                                "\n\tDefault values will be used.")
+                TargetCycle(DEFAULT_YEAR, DEFAULT_SEMESTER)
+            }
+        }
+    }
+}
