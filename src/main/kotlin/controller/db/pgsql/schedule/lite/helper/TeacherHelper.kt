55e1810fa46d33d1527c6a1ad5ebb3fb96987e18
Locked15 <almgal353@gmail.com>
2023-05-26 02:00:46 +0500

    Major updates for Teacher helper class. Now it includes some new anomalies (like reversed additional info (name -> patronymic and vice versa). Also, now it includes teacher name info when an entity is created (to track it after creation). And so on...

--- src/main/kotlin/controller/db/pgsql/schedule/lite/helper/TeacherHelper.kt
+++ src/main/kotlin/controller/db/pgsql/schedule/lite/helper/TeacherHelper.kt
@@ -1,9 +1,11 @@
 package controller.db.pgsql.schedule.lite.helper
 
+import controller.db.pgsql.schedule.lite.helper.util.checkTeacherPropertiesState
+import controller.db.pgsql.schedule.lite.helper.util.filterTeachersListWithoutGenderInclude
 import controller.view.Logger
-import model.data.schedule.base.Teacher
 import model.environment.log.LogLevel
 import org.jetbrains.exposed.dao.id.EntityID
+import model.data.schedule.base.Teacher as TeacherModel
 import model.entity.schedule.lite.base.Teacher as TeacherEntity
 
 
@@ -12,9 +14,9 @@ fun getTeacherWithSideActions(teacherName: String?): Pair<Int?, Boolean> {
         //? To store data in program memory, we convert db-query to a stored list.
         val allAvailableTeachers = TeacherEntity.all().toList()
         //? We must normalize the original sent teacher name, because commonly it has written vary.
-        val normalizedTeacherName = Teacher.normalizeTeacherName(teacherName)
+        val normalizedTeacherName = TeacherModel.normalizeTeacherName(teacherName)
 
-        val newTeacherModel = Teacher.createTeacherModelByNormalizedName(normalizedTeacherName)
+        val newTeacherModel = TeacherModel.createTeacherModelByNormalizedName(normalizedTeacherName)
         tryToFindPresenceEntryID(newTeacherModel, allAvailableTeachers)?.let { Pair(it.value, false) }
             ?: run {
                 try {
@@ -32,48 +34,43 @@ fun getTeacherWithSideActions(teacherName: String?): Pair<Int?, Boolean> {
     }
 }
 
-private fun tryToFindPresenceEntryID(newTeacher: Teacher,
+private fun tryToFindPresenceEntryID(newTeacher: TeacherModel,
                                      allTeachers: List<TeacherEntity>): EntityID<Int>? {
     val fullEqualEntries = allTeachers.filter { it.compareWithOtherModel(newTeacher) == 1 }
     val partialEqualEntries = allTeachers.filter { it.compareWithOtherModel(newTeacher) == 0 }
 
     //? At first, we will check full equality (to make it lazy-working: if there is another instance, other blocks willn't be executed).
-    if (fullEqualEntries.isNotEmpty()) {
+    return if (fullEqualEntries.isNotEmpty()) {
         if (fullEqualEntries.size > 1)
             Logger.logMessage(LogLevel.WARNING, "Teacher entity duplicates was found!\n\tInfo: ${newTeacher.fullName}")
-        return fullEqualEntries[0].id
+        fullEqualEntries[0].id
     }
     //? After the first block (if there are no full equality entries), we will make partial checking.
-    if (partialEqualEntries.size == 1 && checkTeacherPropertiesState(newTeacher, partialEqualEntries[0])) {
+    else if (partialEqualEntries.size == 1 && checkTeacherPropertiesState(newTeacher, partialEqualEntries[0])) {
         partialEqualEntries[0].updateSecondaryFields(newTeacher)
-        return partialEqualEntries[0].id
+        partialEqualEntries[0].id
     }
+    //? In all other cases, we got non-occurred surname (so, we predicate this is new teacher).
+    else {
+        /* But, sometimes in documents surname written in different gender (M -> F), so we should warn user about it.
+           We can't say surely that this is a just mistake (after all, this may be really new teacher).
+           So just make LOG and continue.
 
-    //? Otherwise, there is no searching entry in DB, so we will return null.
-    return null
+           Yeah, this is a reference to gender-bender.
+           Ha-Ha. */
+        val benderTeachers = filterTeachersListWithoutGenderInclude(newTeacher, allTeachers, 1)
+        if (benderTeachers.isNotEmpty()) {
+            val idInfoMessage = "Old ID: ${benderTeachers[0].id} -> New ID: ${allTeachers.maxOf { it.id }.value + 1}"
+            Logger.logMessage(LogLevel.WARNING, "Found teachers (${benderTeachers.size}) with same Surname, " +
+                    "but with different gender ($idInfoMessage)")
+        }
+
+        //? After all, there is no searching entry in DB, so we will return null.
+        null
+    }
 }
 
-private fun checkTeacherPropertiesState(newTeacherEntity: Teacher,
-                                        existTeacherEntity: TeacherEntity): Boolean {
-    val isShortEntriesStateNotEqual = newTeacherEntity.isShortEntry() != existTeacherEntity.isShortEntry()
-    val isAdditionalPropertiesAreReverseEqual = checkSecondaryInfoToReverseEquality(Pair(newTeacherEntity.name,
-                                                                                         newTeacherEntity.patronymic),
-                                                                                    Pair(existTeacherEntity.name,
-                                                                                         existTeacherEntity.patronymic)
-    )
-
-    return isShortEntriesStateNotEqual || isAdditionalPropertiesAreReverseEqual
-}
-
-fun checkSecondaryInfoToReverseEquality(newTeacherEntity: Pair<String?, String?>,
-                                        existingTeacherEntity: Pair<String?, String?>): Boolean {
-    val nameToSurnameEquality = newTeacherEntity.first.equals(existingTeacherEntity.second, true)
-    val patronymicToNameEquality = existingTeacherEntity.first.equals(newTeacherEntity.second, true)
-
-    return nameToSurnameEquality && patronymicToNameEquality
-}
-
-private fun createNewTeacherInstance(newTeacher: Teacher) = TeacherEntity.new {
+private fun createNewTeacherInstance(newTeacher: TeacherModel) = TeacherEntity.new {
     id
     surname = newTeacher.surname
 
