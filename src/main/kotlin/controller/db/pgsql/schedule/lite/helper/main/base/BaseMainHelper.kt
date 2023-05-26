b5d4e50fcd3757853e8903a6e9fc55c00c0f693c
Locked15 <almgal353@gmail.com>
2023-05-26 14:19:29 +0500

    Log message is fixed.

--- src/main/kotlin/controller/db/pgsql/schedule/lite/helper/main/base/BaseMainHelper.kt
+++ src/main/kotlin/controller/db/pgsql/schedule/lite/helper/main/base/BaseMainHelper.kt
@@ -7,8 +7,8 @@ abstract class BaseMainHelper<T>(val newItem: T?) : BaseCommonHelper<T> {
     protected fun getLogMessageForTeachersAltered(alteredTeachers: Pair<Int, List<Pair<Int?, String?>>>,
                                                   targetString: String): String {
         return """New teacher entries: ${alteredTeachers.first} for $targetString
-                    (${joinAlteredTeachersListToString(alteredTeachers.second)})
-        })""".trimIndent()
+            (${joinAlteredTeachersListToString(alteredTeachers.second)})
+        """.trimIndent()
     }
 
     private fun joinAlteredTeachersListToString(info: List<Pair<Int?, String?>>): String {
